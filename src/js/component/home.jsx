47e3ee4a5277340a55b05abc57330088b3f753e6
Freddy2793 <51304209+Freddy2793@users.noreply.github.com>
2023-11-27 17:02:10 +0000

    obtener lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+const [tarea, setTarea]= useState("")
+
+const [ lista, setLista]=useState([])
+
+useEffect(()=>{
+	// crearUsuario()
+	obtenerListaTareas()
+},[])
+
+const crearUsuario=async()=>{
+	const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/freddy",{
+		method:"POST",
+		body:JSON.stringify([]),
+		headers:{"Content-Type":"application/json"}
+	})
+	const data= await response.json()
+	console.log(data)
+}
+
+	const obtenerListaTareas=async()=>{
+
+		try{
+			const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/freddy")
+			const data= await response.json()
+			console.log(data)
+			setTarea(data)
+		}catch(error){
+			console.log(error)
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<input className="form-control" type="text" value={tarea} onChange={(e)=>setTarea(e.target.value)} />
+			<button className="btn btn-success">
+				Agregar tarea
+			</button>
 		</div>
 	);
 };
